c55142b825b23c0d4a55fa88f9c78b250953e869
Xurm1k <kurmoyarcev@gmail.com>
2018-03-26 16:00:59 +0300

    about_strings is_a?

--- koans/about_strings.rb
+++ koans/about_strings.rb
@@ -1,9 +1,9 @@
 require File.expand_path(File.dirname(__FILE__) + '/neo')
 
 class AboutStrings < Neo::Koan
-  def test_double_quoted_strings_are_strings
+  def test_double_quoted_strings_interpolate_variablesquoted_strings_are_strings
     string = "Hello, World"
-    assert_equal __, string.is_a?(String)
+    assert_equal true, string.is_a?(String)
   end
 
   def test_single_quoted_strings_are_also_strings
